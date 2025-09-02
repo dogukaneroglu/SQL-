3dbd01147d28a0ad4fddda5d56932768fa98f7fa
dogukaneroglu <96062775+dogukaneroglu@users.noreply.github.com>
2025-09-02 10:43:07 +0300

    Add files via upload

---
+++ basicsql_4.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT replacement_cost FROM film ;
+SELECT COUNT(DISTINCT replacement_cost) FROM film ; 
+SELECT COUNT(*) FROM film WHERE title LIKE 'T%' AND rating = 'G';
+SELECT count(*) FROM country where country like '_____'; 
+SELECT COUNT(*) FROM city where city like '%r';
